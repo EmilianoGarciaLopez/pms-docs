50418f700f22b691f9d423a83b1e9edb5ef2e631
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2024-03-27 23:45:00 -0400

    first update

--- theme.config.tsx
+++ theme.config.tsx
@@ -18,6 +18,15 @@ const config: DocsThemeConfig = {
   feedback: {
     content: null,
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="PMS Docs" />
+      <meta property="og:description" content="Documentation for PMS products, including PMC-C-CMX" />
+    </>
+  ),
+
+
   footer: {
     text: `© Paisley Microsystems ${currentDate.getFullYear()
       }`,
